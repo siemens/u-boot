a0dd1854f611dc7c6ca9a7316faf7c9d0353e414
Heiko Schocher <hs@denx.de>
2013-09-10 10:33:06 +0200

    video, cfb_console: make background and foreground color configurable
    make CONSOLE_BG_COL/CONSOLE_FG_COL configurable through board config file.
    Clear video screen in video_init().
    
    Signed-off-by: Heiko Schocher <hs@denx.de>
    Cc: Anatolij Gustschin <agust@denx.de>

--- drivers/video/cfb_console.c
+++ drivers/video/cfb_console.c
@@ -2111,6 +2111,24 @@ defined(CONFIG_SANDBOX) || defined(CONFIG_X86)
 	return 0;
 }
 
+void video_clear(void)
+{
+	if (!video_fb_address)
+		return;
+#ifdef VIDEO_HW_RECTFILL
+	video_hw_rectfill(VIDEO_PIXEL_SIZE,	/* bytes per pixel */
+			  0,			/* dest pos x */
+			  0,			/* dest pos y */
+			  VIDEO_VISIBLE_COLS,	/* frame width */
+			  VIDEO_VISIBLE_ROWS,	/* frame height */
+			  bgx			/* fill color */
+	);
+#else
+	memsetl(video_fb_address,
+		(VIDEO_VISIBLE_ROWS * VIDEO_LINE_LEN) / sizeof(int), bgx);
+#endif
+}
+
 static int video_init(void)
 {
 	unsigned char color8;
@@ -2196,6 +2214,8 @@ static int video_init(void)
 		break;
 	}
 	eorx = fgx ^ bgx;
+
+	video_clear();
 
 #ifdef CONFIG_VIDEO_LOGO
 	/* Plot the logo and get start point of console */
@@ -2299,22 +2319,4 @@ int video_get_screen_rows(void)
 int video_get_screen_columns(void)
 {
 	return CONSOLE_COLS;
-}
-
-void video_clear(void)
-{
-	if (!video_fb_address)
-		return;
-#ifdef VIDEO_HW_RECTFILL
-	video_hw_rectfill(VIDEO_PIXEL_SIZE,	/* bytes per pixel */
-			  0,			/* dest pos x */
-			  0,			/* dest pos y */
-			  VIDEO_VISIBLE_COLS,	/* frame width */
-			  VIDEO_VISIBLE_ROWS,	/* frame height */
-			  bgx			/* fill color */
-	);
-#else
-	memsetl(video_fb_address,
-		(VIDEO_VISIBLE_ROWS * VIDEO_LINE_LEN) / sizeof(int), bgx);
-#endif
 }
